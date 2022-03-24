5ecfe1ee34f9bb8155d4cf875556d1e158dbb6f8
Tín <mac@Macs-MacBook-Pro.local>
2022-03-24 13:57:00 +0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import { RNKeycloak, ReactNativeKeycloakProvider } from '../smd';
 import Login from './Login';
 
 const keycloak = new RNKeycloak({
-  url: 'https://keycloak-dev.apps.smartdev.dev/auth',
-  realm: 'jhipster',
-  clientId: 'account-console',
+  url: 'https://keycloak-dev.apps.smartdev.dev/auth/',
+  realm: 'dayoff-dev',
+  clientId: 'dayoff',
 });
 
 export default function App() {
   return (
     <ReactNativeKeycloakProvider
       authClient={keycloak}
-      initOptions={{ redirectUri: '/realms/jhipster/account/*' }}
+      initOptions={{ redirectUri: 'myapp://Homepage' }}
     >
       <Login />
     </ReactNativeKeycloakProvider>
--- src/Login.tsx
+++ src/Login.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { useKeycloak } from '../smd/useKeycloak';
 import styles from './styles';
-
 const Login = () => {
-  const { keycloak } = useKeycloak();
-
+  const { keycloak, initialized } = useKeycloak();
+  console.log('dasyss', keycloak);
+  
   return (
     <View style={styles.container}>
-      <Text>{`Welcome ${keycloak?.authenticated} - ${keycloak?.token}!`}</Text>
+      <View>
+      <Text>{`Welcome ${keycloak?.authenticated ? keycloak.tokenParsed.email : 'You need to login'} - ${keycloak?.token}!`}</Text>
       <Button onPress={() => keycloak?.login()} title="Login" />
+      
+      {!!keycloak.authenticated && (
+        <Button onPress={() => keycloak.logout()} title="Logout" />
+      )}
+    </View>
     </View>
   );
 };
